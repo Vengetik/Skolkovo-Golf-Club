11236631fec723f24c31b298d88fedf44af99be0
Vengetik <vengetik@gmail.com>
2018-09-09 19:26:43 +0300

    fixed mobile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-const voteButton = document.querySelector('.main__button');
+const sendButton = document.querySelector('.main__button');
 const voteModal = document.querySelector('.modal__vote');
 const modalLayer = document.querySelector('.modals__layer-around');
 const modalForm = document.querySelector('.vote__form');
@@ -9,12 +9,20 @@ const mainModals = document.querySelector('.main__modals');
 const checkbox = document.querySelectorAll('input[type="checkbox"]');
 const voteCard = document.querySelectorAll('.card__image');
 
+const voteButtonChoose = document.querySelectorAll('.vote__button--choose');
+const voteButtonMore = document.querySelectorAll('.vote__button--more');
+
 const chooseTrainer = () => {
   for (let i = 0; i < checkbox.length; i++) {
     if (checkbox[i].checked) {
       voteCard[i].classList.add('checked');
     } else {
       voteCard[i].classList.remove('checked');
+    }
+    if(document.getElementsByClassName("marked").length>1){
+      alert("Please select only one check box");
+      checkbox[i].checked=false;
+      checkbox[i].classList.remove("checked");
     }
   }
 };
@@ -29,7 +37,7 @@ document.addEventListener('keydown', function onModalClose(evt) {
   }
 });
 
-voteButton.addEventListener('click', function onVoteButtonClick(evt) {
+sendButton.addEventListener('click', function onVoteButtonClick(evt) {
   mainModals.classList.toggle('hidden');
 });
 
@@ -47,3 +55,6 @@ modalSubmit.addEventListener('click', function (evt) {
 
   voteModal.insertBefore(h1, voteModal.children[1]);
 });
+
+
+
