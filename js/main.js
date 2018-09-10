39df281d08e9fe03a153ad1760ff0c359ea3f486
Vengetik <vengetik@gmail.com>
2018-09-10 17:16:19 +0300

    Added modals

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 const sendButton = document.querySelector('.main__button');
 const voteModal = document.querySelector('.modal__vote');
-const modalLayer = document.querySelector('.modals__layer-around');
+const modalLayer = document.querySelectorAll('.modals__layer-around');
 const modalForm = document.querySelector('.vote__form');
 const modalSubmit = modalForm.querySelector('input[type="submit"]');
 const modalHeading = voteModal.querySelector('.vote__heading');
@@ -9,8 +9,10 @@ const mainModals = document.querySelector('.main__modals');
 const checkbox = document.querySelectorAll('input[type="checkbox"]');
 const voteCard = document.querySelectorAll('.card__image');
 
-const voteButtonChoose = document.querySelectorAll('.vote__button--choose');
 const voteButtonMore = document.querySelectorAll('.vote__button--more');
+
+const trainerModals = document.querySelector('.trainer__modals');
+const trainerModalsContent = document.querySelectorAll('.trainer__content');
 
 const chooseTrainer = () => {
   for (let i = 0; i < checkbox.length; i++) {
@@ -19,13 +21,23 @@ const chooseTrainer = () => {
     } else {
       voteCard[i].classList.remove('checked');
     }
-    if(document.getElementsByClassName("marked").length>1){
-      alert("Please select only one check box");
-      checkbox[i].checked=false;
-      checkbox[i].classList.remove("checked");
+    if(document.getElementsByClassName("checked").length>1){
+      alert("Поэалуйста, выберите только одного тренера.");
+      checkbox[i].checked = false;
+      voteCard[i].classList.remove("checked");
     }
   }
 };
+
+const hideTrainerModal = () => {
+  trainerModals.classList.add('hidden');
+  trainerModalsContent.forEach( (it) => {
+    if (!it.classList.contains('hidden')) {
+      it.classList.add('hidden');
+    }
+  })
+};
+
 checkbox.forEach( (it) => {
   it.addEventListener('click', chooseTrainer);
 });
@@ -33,7 +45,7 @@ checkbox.forEach( (it) => {
 document.addEventListener('keydown', function onModalClose(evt) {
   if (evt.keyCode === 27) {
     mainModals.classList.add('hidden');
-    document.removeEventListener('keydown', onModalClose);
+    hideTrainerModal();
   }
 });
 
@@ -41,8 +53,13 @@ sendButton.addEventListener('click', function onVoteButtonClick(evt) {
   mainModals.classList.toggle('hidden');
 });
 
-modalLayer.addEventListener('click', function onModalLayerClick() {
-  mainModals.classList.toggle('hidden');
+modalLayer.forEach((it) => {
+  it.addEventListener('click', function onModalLayerClick() {
+    if (!mainModals.classList.contains('hidden')) {
+      mainModals.classList.add('hidden');
+    }
+    hideTrainerModal();
+})
 });
 modalSubmit.addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -51,10 +68,21 @@ modalSubmit.addEventListener('click', function (evt) {
   modalSubmit.style.display = 'none';
   const h1 = document.createElement('h1');
   h1.innerHTML = 'Вы успешно отправили голос!';
-  h1.style.marginTop = '20%';
+
 
   voteModal.insertBefore(h1, voteModal.children[1]);
 });
 
-
-
+voteButtonMore.forEach((it, index) => {
+  it.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    trainerModals.classList.toggle('hidden');
+    trainerModalsContent[index].classList.toggle('hidden');
+  })
+});
+const voteTrainerButton = document.querySelector('.vote__button--trainer');
+voteTrainerButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  hideTrainerModal();
+  mainModals.classList.toggle('hidden');
+});
